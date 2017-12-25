b36f14bcc3d339438cb7e79e6963972585640e64
Gene Boggs <gene.boggs@gmail.com>
2017-12-24 23:16:28 -0800

    Whitespace!

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ shinyUI(
                 radioButtons(
                     'graph_type',
                     label = h3('Graph type'),
-                    choices = list('Line' = 'l', 'Point' = 'p'),
+                    choices = list( 'Line' = 'l', 'Point' = 'p' ),
                     selected = 'p'
                 ),
 
