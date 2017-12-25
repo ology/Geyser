8357879983452e42c064b9fd4f1c49a20284e7fa
Gene Boggs <gene.boggs@gmail.com>
2017-12-24 20:34:51 -0800

    Improve variable name

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(
             sidebarPanel(
 
                 radioButtons(
-                    'type',
+                    'graph_type',
                     label = h3('Graph type'),
                     choices = list('Line' = 'l', 'Point' = 'p'),
                     selected = 'p'
