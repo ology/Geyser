ccecb4f86efd7f14296a23c06bb21b972f133460
Gene Boggs <gene.boggs@gmail.com>
2018-04-07 21:18:17 -0700

    Comment-out redundant code

--- ui.R
+++ ui.R
@@ -10,12 +10,13 @@ shinyUI(
         sidebarLayout(
             sidebarPanel(
 
-                radioButtons(
-                    'graph_type',
-                    label = h3('Graph type'),
-                    choices = list( 'Line' = 'l', 'Point' = 'p' ),
-                    selected = 'p'
-                ),
+# XXX Redundant:
+#                radioButtons(
+#                    'graph_type',
+#                    label = h3('Graph type'),
+#                    choices = list( 'Line' = 'l', 'Point' = 'p' ),
+#                    selected = 'p'
+#                ),
 
                 checkboxInput( 'arrow_toggle', 'Show arrows', FALSE ),
 
